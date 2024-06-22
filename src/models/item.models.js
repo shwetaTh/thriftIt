3e1ce3ebca717e30eb463457c4220687a7a34a0a
shwetaTh <shwetaTh@users.noreply.github.com>
2024-06-22 20:25:24 +0545

    created item model

---
+++ src/models/item.models.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  condition: {
+    type: String,
+    required: true,
+    enum: ['New', 'Used']
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, { timestamps: true });
+
+export const Item = mongoose.model('Item', itemSchema);
